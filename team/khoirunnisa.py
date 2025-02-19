fc6e9b597b9037a174715ed64ffb23309c25813d
Khrns888 <niscahairunnisa@gmail.com>
2025-02-19 21:57:02 +0700

    Create khoirunnisa.py

---
+++ team/khoirunnisa.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Khoirunnisa"
+    nim = "2213010500"
+    peran = "edit video dan dubbing suara"
+    perkenalan(nama, nim, peran)

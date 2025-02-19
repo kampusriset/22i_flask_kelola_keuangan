28725c48340cf7cca5c400bbbc9a9159822b61b4
dzzul <138375199+dzzul@users.noreply.github.com>
2025-02-19 12:58:36 +0700

    Create izzul.py

---
+++ team/izzul.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Muhammad 'izzul Islam"
+    nim = "2213010485"
+    peran = "Mengerjakan Memperbaiki tampilan serta menjadi helper"
+    perkenalan(nama, nim, peran)

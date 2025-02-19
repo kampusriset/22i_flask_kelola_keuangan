1178f9cfd7cb6b1e27232c1ceef8681200c676a4
NurcahyoSaputro <80509850+NurcahyoSaputro@users.noreply.github.com>
2025-02-19 12:23:39 +0700

    Create cahyo.py

---
+++ team/cahyo.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Nurcahyo Saputro"
+    nim = "2213010475"
+    peran = "Mengerjakan login, register, change password, log out, file model"
+    perkenalan(nama, nim, peran)

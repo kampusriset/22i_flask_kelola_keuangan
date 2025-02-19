ebbb03f10484f48c6c3f2508f7a617991cf55dd1
Dicky Kurniawan <dickykurniawan102004@gmail.com>
2025-02-19 12:55:30 +0700

    Create Dicky.py

---
+++ team/Dicky.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Dicky kurniawan"
+    nim = "2213010470"
+    peran = "halaman Utama dan Controller crud"
+    perkenalan(nama, nim, peran)
